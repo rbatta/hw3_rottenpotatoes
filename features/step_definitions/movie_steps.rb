eb1b12185f0f90c8e6e8953c3079f7f56bffe288
Risa Batta <rbatta@gmail.com>
2013-08-05 12:14:19 -0700

    added functionality to check/uncheck ratings

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,8 +2,7 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    #debugger
-    Movie.create(movie)# each returned element will be a hash whose key is the table header.
+    Movie.create!(movie) # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
   #flunk "Unimplemented"
@@ -23,10 +22,25 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  
+  debugger
+  if !uncheck
+    rating_list.split(', ').each do |x|
+      #word = "ratings_#{x}"
+      #check(word)
+      step("I check \"ratings_#{x}\"") 
+    end
+  else
+    rating_list.split(', ').each do |x|
+      step("I uncheck \"ratings_#{x}\"")
+      #uncheck("ratings_#{x}")
+    end
+    save_and_open_page
+  end
+
+  #end # gets array of the ratings
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  #When /^(?:|I )check "([^"]*)"$/ do |field|
-  #  check(field)
-  #end
+
 end
